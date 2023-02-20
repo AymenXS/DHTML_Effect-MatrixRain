e629933000b7e1d8306a7c08774da0006d05fe43
AymenXS <aymenghaloua@gmail.com>
2023-02-20 12:19:59 +0100

    Updating

--- script.js
+++ script.js
@@ -14,8 +14,14 @@ class Symbol {
     this.canvasHeight = canvasHeight;
   }
 
-  draw() {
+  draw(context) {
     this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+    context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+      this.y = 0;
+    } else {
+      this.y += 1;
+    }
   }
 }
 
@@ -26,13 +32,52 @@ class Effect {
     this.fontSize = 25;
     this.columns = this.canvasWidth / this.fontSize;
     this.symbols = [];
+    this.#initialize();
   }
 
   #initialize() {
-    for (let i = 0; i < this.columns.length; i++) {
-      this.symbols[i] = new Symbol();
+    for (let i = 0; i < this.columns; i++) {
+      this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
     }
+  }
+
+  resize(width, height) {
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.#initialize();
   }
 }
 
-function animate() {}
+const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 144;
+const nextFrame = 1000 / fps;
+let timer = 0;
+
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if (timer > nextFrame) {
+    ctx.fillStyle = "rgba(0,0,0,0.05)";
+    ctx.textAlign = "center";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#0aff0a";
+    ctx.font = effect.fontSize + "px monospace"; // "monospace" font have characters that occupy the same amount of space
+
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+    timer = 0;
+  } else {
+    timer += deltaTime;
+  }
+  requestAnimationFrame(animate);
+
+  window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height);
+  });
+}
+
+animate(0);
