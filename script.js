3e4c7e1a6d666aab4aca7b1e768406c93290dbcc
AymenXS <aymenghaloua@gmail.com>
2023-02-20 10:01:57 +0100

    Updating

---
+++ script.js
@@ -0,0 +1,38 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol {
+  constructor(x, y, fontSize, canvasHeight) {
+    this.characters =
+      "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.text = "";
+    this.canvasHeight = canvasHeight;
+  }
+
+  draw() {
+    this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+  }
+}
+
+class Effect {
+  constructor(canvasWidth, canvasHeight) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.fontSize = 25;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+  }
+
+  #initialize() {
+    for (let i = 0; i < this.columns.length; i++) {
+      this.symbols[i] = new Symbol();
+    }
+  }
+}
+
+function animate() {}
